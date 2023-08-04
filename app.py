e1300543af87d8c948a78b146629948949b19975
unknown <ccmilne@umich.edu>
2023-08-04 14:16:14 -0400

    Finished app

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import streamlit
 import pandas as pd
 import os
 
-def generate_response(txt):
-    pass
+from langchain.llms import OpenAI
+from langchain.agents import create_csv_agent
 
-def check_file_type(file):
-    '''
-    Returns file
-    '''
-    return os.path.splitext(file.name)[1]
+#API KEY
+openai_key = os.getenv("OPENAI_API_KEY")
+
+def csv_agent(file):
+    return create_csv_agent(llm=OpenAI(temperature=0), path=file, verbose=False)
+
 
 #Webpage
 streamlit.set_page_config(page_title="Chat with your Excel Document", page_icon=":random:", layout='centered')
@@ -18,7 +19,6 @@ streamlit.title('Chat with your Excel Document')
 # #Take an input
 # txt_input = streamlit.text_area('Enter your text to summarize', '', height=200)
 user_file = streamlit.file_uploader("Upload file", type={"csv", "excel"}, accept_multiple_files=False)
-streamlit.text_area("Ask the dataset a question", "", placeholder="Example: How many rows are there?\nExample: What % of the rows contain nulls?")
 
 if user_file is not None:
     streamlit.write(f"Uploaded Dataset: {os.path(user_file)}")
@@ -26,14 +26,28 @@ if user_file is not None:
         df = pd.read_csv(user_file)
     elif os.path.splitext(user_file.name)[1] == '.xlsx':
         df = pd.read_excel(user_file)
+    streamlit.write(df)
+    agent = csv_agent(file=user_file)
 
 else:
     url = "https://www.kaggle.com/datasets/sdolezel/black-friday?resource=download"
     streamlit.write(f"Placeholder Dataset: [Black Friday Sales]({url})")
     df = pd.read_csv('data/test.csv')
+    streamlit.write(df)
+    agent = csv_agent(file="data/test.csv")
 
-streamlit.write(df)
+question = streamlit.text_area("Ask the data a question", "", placeholder="Example: How many rows are there?\nExample: What % of the rows contain nulls?")
 
+###Conversational part
+with streamlit.form("answer_form", clear_on_submit=True):
+    #openai_api_key = streamlit.text_input('OpenAI API Key', type='password', disabled=not question, value=streamlit.secrets['OPENAI_API_KEY'])
+    submitted = streamlit.form_submit_button("SUBMIT")
+
+    if submitted and openai_key:
+        with streamlit.spinner("Answering ..."):
+            response = agent.run(question)
+            if response:
+                streamlit.write(response)
 
 # #Form to accept input
 # result = []
